be0ca393a88c270082d032fe8d84326dda4e8758
devGiyosiddin <devgiyosiddin@gmail.com>
2025-06-04 14:50:04 +0500

    some chnages

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -17,15 +17,15 @@ const Header = () => {
             <Link to="/" className="text-2xl font-bold text-blue-600">
                 <span className='flex items-center gap-3'>
                     <img className="bg-lime-100 rounded-full" src="/public/logo-3.png" alt="Hashira website logo" width={64} height={64} srcSet="" />
-                    <span className="text-lime-600">Hashira</span>
+                    <span className="text-lime-600 text-4xl">Hashira</span>
                 </span>
             </Link>
             <nav className="space-x-4 text-sm sm:text-base">
                 <Link to="/anime" className="text-gray-700 hover:text-blue-500">Anime</Link>
                 <Link to="/manga" className="text-gray-700 hover:text-blue-500">Manga</Link>
-                <Link to="/news" className="text-gray-700 hover:text-blue-500">Yangiliklar</Link>
+                <Link to="/yangiliklar" className="text-gray-700 hover:text-blue-500">Yangiliklar</Link>
                 <Link to="/forum" className="text-gray-700 hover:text-blue-500">Forum</Link>
-                <Link to="/shop" className="text-gray-700 hover:text-blue-500">Market</Link>
+                <Link to="/market" className="text-gray-700 hover:text-blue-500">Market</Link>
             </nav>
             <label htmlFor="searchInput">
                 <input ref={inputRef}
@@ -34,6 +34,7 @@ const Header = () => {
                     onChange={handleSearch}
                 />
             </label>
+            <button type='button'>Kirish</button>
         </header>
     )
 }
