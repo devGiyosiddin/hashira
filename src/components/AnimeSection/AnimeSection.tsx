e72762493b56a9819a24153fe7ad5de28153b97f
devGiyosiddin <devgiyosiddin@gmail.com>
2025-07-03 23:32:32 +0500

    style: улучшено оформление сетки в компоненте AnimeSection для лучшей адаптивности

--- src/components/AnimeSection/AnimeSection.tsx
+++ src/components/AnimeSection/AnimeSection.tsx
@@ -15,16 +15,16 @@ interface AnimeSectionProps {
 
 export const AnimeSection = ({ title, data, isLoading, error, icon }: AnimeSectionProps) => (
     <Section title={title} icon={icon}>
-    {isLoading ? (
-        <LoadingSpinner size="lg" />
-    ) : error ? (
-        <ErrorMessage message={error.message} />
-    ) : (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6">
-        {data?.map((item: Anime) => (
-            <AnimeCard key={item.mal_id} item={item} />
-        ))}
-        </div>
-    )}
+        {isLoading ? (
+            <LoadingSpinner size="lg" />
+        ) : error ? (
+            <ErrorMessage message={error.message} />
+        ) : (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-6 gap-4 justify-items-center max-w-[1400px] mx-auto">
+                {data?.map((item: Anime) => (
+                    <AnimeCard key={item.mal_id} item={item} />
+                ))}
+            </div>
+        )}
     </Section>
 );
