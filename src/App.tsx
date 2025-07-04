f1e08ec6239282286aaeeb9c1ce8fb9bdcc5dd07
devGiyosiddin <devgiyosiddin@gmail.com>
2025-07-04 12:38:05 +0500

    fix: исправлено оформление основного контента в компоненте App для улучшения структуры

--- src/App.tsx
+++ src/App.tsx
@@ -11,19 +11,21 @@ function App() {
   return (
       <div className="min-h-screen px-4 sm:px-6 lg:px-8 pt-[100px]">
       <SiteHeader />
-        {/* Main content area */}
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path='/search' element={<Search />}></Route>
-          <Route path="/anime/:id" element={<AnimeDetailPage />} />
-          <Route path="/anime" element={<div>Anime List Page</div>} />
-          <Route path="/manga" element={<div>Manga Page</div>} />
-          <Route path="/news" element={<div>News Page</div>} />
-          <Route path="/forum" element={<div>Forum Page</div>} />
-          <Route path="/search" element={<div>Search Page</div>} />
-          <Route path="/all" element={<AllAnime />} />
-          <Route path="/watch/:animeId/:episodeNumber" element={<WatchPage />} />
-        </Routes>
+      {/* Main content area */}
+        <main className='flex flex-col items-center justify-center min-h-screen'>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path='/search' element={<Search />}></Route>
+            <Route path="/anime/:id" element={<AnimeDetailPage />} />
+            <Route path="/anime" element={<div>Anime List Page</div>} />
+            <Route path="/manga" element={<div>Manga Page</div>} />
+            <Route path="/news" element={<div>News Page</div>} />
+            <Route path="/forum" element={<div>Forum Page</div>} />
+            <Route path="/search" element={<div>Search Page</div>} />
+            <Route path="/all" element={<AllAnime />} />
+            <Route path="/watch/:animeId/:episodeNumber" element={<WatchPage />} />
+          </Routes>
+        </main>
     </div>
     
     // TODO: Add Footer component
